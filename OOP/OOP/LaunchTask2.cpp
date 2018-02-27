4a43fa86eec0456096f88c9cfef4c47cb9455851
lKrausz <krausz.st@gmail.com>
2018-02-28 02:17:16 +0700

    lab2 update, add all functions

--- OOP/OOP/LaunchTask2.cpp
+++ OOP/OOP/LaunchTask2.cpp
@@ -17,10 +17,6 @@ void LaunchTask2()
 int Menu2()
 {
 	int key;
-	double x1;
-	double x2;
-	int value1 = 0;
-	int value2 = 0;
 	double z = 0;
 
 
@@ -33,15 +29,13 @@ int Menu2()
 			<< "1. ""Hello, World!"";" << endl
 			<< "2. Calculator;" << endl
 			<< "3. Quadratic equation;" << endl
-			<< "4. Quadratic equation (2);" << endl
-			<< "5. Pointer;" << endl
-			<< "6. Even or Odd;" << endl
-			<< "7. multiplicity of number (5 and 2);" << endl
-			<< "8. PredCondition;" << endl
-			<< "9. PostCondition;" << endl
-			<< "A. Iteration;" << endl
-			<< "B. Break;" << endl
-			<< "C. Float <--> Int;" << endl
+			<< "4. Quadratic equation (links);" << endl
+			<< "5. Sum of numbers;" << endl
+			<< "6. Global value;" << endl
+			<< "7. Exponentiation;" << endl
+			<< "8. 'Guess the Number' game;" << endl
+			<< "9. Sorts;" << endl
+			<< "10. Multiply Matrix;" << endl <<endl
 			<< "0. Exit." << endl;
 
 		cin >> key;
@@ -55,12 +49,14 @@ int Menu2()
 
 		case 2:
 			{
+			int value1 = 0;
+			int value2 = 0;
 			char operationKey;
 			cout << "Enter mathematic operation (+, -, *, /, %): ";
 			operationKey = getchar();
 			while ((operationKey != '+') && (operationKey != '-') && (operationKey != '*') && (operationKey != '/') && (operationKey != '%'))
 			{
-				printf("\nINCORRECT SYMBOL!!!\nPlease, enter (+, -, *, /, %):");
+				cout << "INCORRECT SYMBOL!!! Please, enter (+, -, *, /, %):" << endl;
 				operationKey = getchar();
 				fflush(stdin);
 			}
@@ -71,76 +67,194 @@ int Menu2()
 
 		case 3:
 		{
-			x1 = 0;
-			x2 = 0;
+			double x1 = 0;
+			double x2 = 0;
+			double x3 = 0;
+			double x4 = 0;
 			double* x1Pointer = &x1;
 			double* x2Pointer = &x2;
 			check = GetRoots(1, 3, 2, x1Pointer, x2Pointer);
-			DisplayRootsValues(check, x1Pointer, x2Pointer);
+			DisplayRootsValues(check, *x1Pointer, *x2Pointer);
 			check = GetRoots(1, 4, 0, x1Pointer, x2Pointer);
-			DisplayRootsValues(check, x1Pointer, x2Pointer);
+			DisplayRootsValues(check, *x1Pointer, *x2Pointer);
 			check = GetRoots(0, 1, 2, x1Pointer, x2Pointer);
-			DisplayRootsValues(check, x1Pointer, x2Pointer);
+			DisplayRootsValues(check, *x1Pointer, *x2Pointer);
 			check = GetRoots(0, 0, 3, x1Pointer, x2Pointer);
-			DisplayRootsValues(check, x1Pointer, x2Pointer);
+			DisplayRootsValues(check, *x1Pointer, *x2Pointer);
 			check = GetRoots(0, 1, 0, x1Pointer, x2Pointer);
-			DisplayRootsValues(check, x1Pointer, x2Pointer);
+			DisplayRootsValues(check, *x1Pointer, *x2Pointer);
 			check = GetRoots(4, 1, 4, x1Pointer, x2Pointer);
-			DisplayRootsValues(check, x1Pointer, x2Pointer);
+			DisplayRootsValues(check, *x1Pointer, *x2Pointer);
 			break;
 		}
 
 		case 4:
 		{
-			x1 = 0;
-			x2 = 0;
+			double x1 = 0;
+			double x2 = 0;
 			double& x1Reference = x1;
 			double& x2Reference = x2;
 			check = AnotherGetRoots(1, 3, 2, x1Reference, x2Reference);
-			AnotherDisplayRootsValues(check, x1Reference, x2Reference);
+			DisplayRootsValues(check, x1, x2);
 			check = AnotherGetRoots(1, 4, 0, x1Reference, x2Reference);
-			AnotherDisplayRootsValues(check, x1Reference, x2Reference);
+			DisplayRootsValues(check, x1, x2);
 			check = AnotherGetRoots(0, 1, 2, x1Reference, x2Reference);
-			AnotherDisplayRootsValues(check, x1Reference, x2Reference);
+			DisplayRootsValues(check, x1, x2);
 			check = AnotherGetRoots(0, 0, 3, x1Reference, x2Reference);
-			AnotherDisplayRootsValues(check, x1Reference, x2Reference);
+			DisplayRootsValues(check, x1, x2);
 			check = AnotherGetRoots(0, 1, 0, x1Reference, x2Reference);
-			AnotherDisplayRootsValues(check, x1Reference, x2Reference);
+			DisplayRootsValues(check, x1, x2);
 			check = AnotherGetRoots(4, 1, 4, x1Reference, x2Reference);
-			AnotherDisplayRootsValues(check, x1Reference, x2Reference);
+			DisplayRootsValues(check, x1, x2);
 			break;
 		}
 
-		case '5':
-
+		case 5:
+			{
+			int a = 1;
+			int b = 2;
+			SummNumbers(a, b);
+			double x = 3.0;
+			double y = 4.0;
+			SummNumbers(x, y);
+			SummNumbers(a, y);
+			float m = 5.0;
+			float n = 6.0;
+			SummNumbers(m, n);
+			}
 			break;
 
-		case '6':
-
+		case 6:
+			{
+			cout << "Global Variable: "<< globalVariable << endl;
+			GlobalPlusTwo();
+			cout << "Global Variable + 2: " << globalVariable << endl;
+			GlobalMultiplyThree();
+			cout << "Global Variable * 3: " << globalVariable << endl;
+			GlobalEqualsOne();
+			cout << "New global Variable: " << globalVariable << endl;
+			globalVariable = 5;
+			cout << "New global Variable: " << globalVariable << endl;
+			}
 			break;
 
-		case '7':
-
+		case 7:
+			{
+			int n;
+			int m;
+			cout << "Enter the base: " << endl;
+			cin >> n;
+			cout << "Enter the power: " << endl;
+			cin >> m;
+			cout << n << " in power " << m << " = " << GetPower(n, m) << endl;
+			}
 			break;
 
-		case '8':
-
+		case 8:
+			{
+			Game();
+			}
 			break;
 
-		case '9':
+		case 9:
+		{
+			int n;
+			int sortKey;
+			cout << "Enter the size of the array:" << endl;
+			cin >> n;
+			double* array = new double[n];
+			srand(time(NULL));
+			cout << "Your array: " << endl;
+			for (int i = 0; i < n; i++) 
+			{
+				array[i] = rand() % 100;
+				cout << array[i] << " ";
+			}
+			cout << "Enter sort number: " << endl 
+			<< "1- Bubble Sort;" << endl 
+			<< "2- Quick Sort;" << endl 
+			<< "3- Selection Sort" << endl;
+			cin >> sortKey;
+			if (sortKey == 1)
+			{
+				cout << "BubbleSort results: " << endl;
+				double *arrPointer = array;
+				BubbleSort(arrPointer, n);
+				ArrayShow(array, n);
+			}
+			if (sortKey == 2)
+			{
+				cout << "QuickSort results: " << endl;
+				QuickSort(array, n);
+				ArrayShow(array, n);
+			}
+			if (sortKey == 3)
+			{
+				cout << "SelectionSort results: " << endl;
+				SelectionSort(array, n);
+				ArrayShow(array, n);
+			}
 
+			delete[] array;
+		}
 			break;
 
-		case 'A':
-
-			break;
-
-		case 'B':
-
-			break;
-
-		case 'C':
-
+		case 10:
+			{
+			//Инициализация матрицы А
+			int n;
+			int m;
+			cout << "Enter the number of rows of matrix A" << endl;
+			cin >> n;
+			cout << "Enter the number of columns of matrix A" << endl;
+			cin >> m;
+			srand(time(NULL));
+			int **aMatrix = new int*[n];
+			cout << "The array A is" << endl;
+			aMatrix = MatrixValues(n, m, aMatrix);
+			//Инициализация матрицы В
+			int w;
+			cout << "The number of rows of matrix B equal the number of columns of matrix A: " << m << endl;
+			cout << "Enter the number of columns of matrix B" << endl;
+			cin >> w;
+			int **bMatrix = new int*[m];
+			cout << "The array B is" << endl;
+			bMatrix = MatrixValues(m, w, bMatrix);
+			//Инициализация матрицы результата
+			int** result = new int*[n];
+			for (int i = 0; i < n; i++)
+			{
+				result[i] = new int[w];
+			}
+			for (int i = 0; i< n; i++)
+			{
+				for (int j = 0; j < w; j++)
+				{
+					result[i][j] = 0;
+				}
+				cout << endl;
+			}
+			//Умножение матриц и вывод резульатата на экран
+			result = MultiplyMatrix(n, w, m, aMatrix, bMatrix, result);
+			cout << "Results: " << endl;
+			for (int i = 0; i< n; i++)
+			{
+				for (int j = 0; j < w; j++)
+				{
+					cout << result[i][j] << ' ';
+				}
+				cout << endl;
+			}
+			for (int i = 0; i < n; i++)
+			delete[] aMatrix[i];
+			delete[] aMatrix;
+			for (int i = 0; i < z; i++)
+			delete[] bMatrix[i];
+			delete[] bMatrix;
+			for (int i = 0; i < n; i++)
+			delete[] result[i];
+			delete[] result;
+			}
 			break;
 
 		case 0:
--- OOP/OOP/LaunchTask2Functions.h
+++ OOP/OOP/LaunchTask2Functions.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <ctime>
+#include <iomanip>
 
 using namespace std;
 
@@ -28,8 +29,8 @@ double MakeCalculation(int value1, int value2, char operationKey)
 }
 
 
-//Ввод данных для функций поиска корней квадратного уравнения
-void DisplayRootsValues(int check, double* x1, double* x2)
+//Вывод данных для функций поиска корней квадратного уравнения
+void DisplayRootsValues(int check, double x1, double x2)
 {
 	if (check == 0)
 	{
@@ -37,19 +38,18 @@ void DisplayRootsValues(int check, double* x1, double* x2)
 	}
 	if (check == 1)
 	{
-		cout << "Real roots: x1 = x2: " << *x1 << endl << endl;
+		cout << "Real roots: x1 = x2 = " << x1 << endl << endl;
 	}
 	if (check == 2)
 	{
-		cout << "Real roots: "<< endl << "x1: " << *x1 << endl << "x2: " << *x2 << endl << endl;
+		cout << "Real roots:" << endl << "x1 = " << x1 << endl << "x2: " << x2 << endl << endl;
 	}
 	if (check == -1)
 	{
-		cout << "Complex roots: "<< endl <<"x1 = " << *x1 << " + " << *x2 << "i" << endl;
-		cout << "x2 = " << *x1 << " - " << *x2 << "i" << endl << endl;
+		cout << "Complex roots:" << endl << "x1 = " << x1 << " + " << x2 << "i" << endl;
+		cout << "x2 = " << x1 << " - " << x2 << "i" << endl << endl;
 	}
 }
-
 
 //Корни квадратного уравнения
 int GetRoots(int a, int b, int c, double* x1, double* x2)
@@ -83,31 +83,6 @@ int GetRoots(int a, int b, int c, double* x1, double* x2)
 	}
 	return 0;
 }
-
-
-//Ввод данных для функций поиска корней квадратного 
-//уравнения с использованием ссылок
-void AnotherDisplayRootsValues(int check, double& x1, double& x2)
-{
-	if (check == 0)
-	{
-		cout << "Have no roots" << endl << endl;
-	}
-	if (check == 1)
-	{
-		cout << "Real roots: x1 = x2 = " << x1 << endl << endl;
-	}
-	if (check == 2)
-	{
-		cout << "Real roots:" << endl <<"x1 = " << x1 << endl <<"x2: " << x2 << endl << endl;
-	}
-	if (check == -1)
-	{
-		cout << "Complex roots:" << endl << "x1 = " << x1 << " + " << x2 << "i" << endl;
-		cout << "x2 = " << x1 << " - " << x2 << "i" << endl << endl;
-	}
-}
-
 
 //Корень квадратного уравнения (с использованием ссылок)
 int AnotherGetRoots(int a, int b, int c, double& x1, double& x2)
@@ -146,21 +121,19 @@ int AnotherGetRoots(int a, int b, int c, double& x1, double& x2)
 //Рассчитать сумму двух целочисленных переменных (перегрузка функций)
 void SummNumbers(int value1, int value2)
 {
-	printf("Summ of integer is %d\n", value1 + value2);
+	cout << "Summ of integers is " << value1 + value2 << endl;
 }
 
-
-//Рассчитать сумму двух целочисленных переменных
+//Рассчитать сумму двух вещественных переменных
 void SummNumbers(double value1, double value2)
 {
-	printf("Summ of double is %f\n", value1 + value2);
+	cout << fixed << setprecision(2) << "Summ of doubles is " << value1 + value2 << endl;
 }
 
-
-//Рассчитать сумму двух целочисленных переменных
+//Рассчитать сумму целочисленного и вещественного переменных
 void SummNumbers(int value1, double value2)
 {
-	printf("Summ of integer and double is %f\n", value1 + value2);
+	cout << "Summ of integer and double is " << value1 + value2 << endl;
 }
 
 
@@ -178,6 +151,7 @@ void GlobalEqualsOne()
 	globalVariable = 1;
 }
 
+
 //Рекурсивный поиск числа в степени
 int GetPower(int base, int power)
 {
@@ -191,23 +165,161 @@ int GetPower(int base, int power)
 	}
 }
 
+
 //Игра на угадывание рандомно заданного числа. 
-//Изменить: 5 попыток, указывать, больше число или меньше введенного пользователем
+//3 попытки, указывается, больше число или меньше введенного пользователем
 int Game()
 {
 	srand(time(NULL)); // для задания случайного начального числа
-	printf("\n---Game: Guess the Number---\n");
+	cout << "---Game: Guess the Number---" << endl;
 	int guessNumber = rand() % 10; // генерация угадываемого числа
+	cout << guessNumber << endl;
 	int enteredNumber = -1; // вводимое пользователем число
-	int shots = 0; // количество попыток
-	printf("\nEnter number from 0 to 9: ");
-	scanf_s("%d", &enteredNumber);
-	while (guessNumber != enteredNumber)
+	int shots = 1; // количество попыток
+	cout << "Enter number from 0 to 9: ";
+	cin >> enteredNumber;
+
+	while (shots <= 3)
 	{
+		if (guessNumber != enteredNumber)
+		{
+			if (shots == 3)
+			{
+				cout << "Wrong!!! You lose!" << endl;
+			}
+			else
+			{
+				cout << "Wrong!!! Try again!" << endl;
+				if (guessNumber < enteredNumber) cout << "Number less then " << enteredNumber << endl;
+				else cout << "Number more then " << enteredNumber << endl;
+				cin >> enteredNumber;
+			}
+		}
+		else
+		{
+			cout << "\nCorrect! You win in "<< shots << " shots!\n" << endl;
+			break;
+		}
 		shots++;
-		printf("\nWrong!!! Try again!\nEnter number from 0 to 9: ");
-		scanf_s("%d", &enteredNumber);
 	}
-	printf("\nCorrect! You win in %d shots!\n", shots);
 	return 0;
+}
+
+
+//Сортировка пузырьком
+void BubbleSort(double *arrPointer, int n)
+{
+	double tmp = 0;
+	for (int i = 0; i<n; i++) {
+		for (int j = (n - 1); j >= (i + 1); j--) {
+			if (arrPointer[j]<arrPointer[j - 1]) {
+				tmp = arrPointer[j];
+				arrPointer[j] = arrPointer[j - 1];
+				arrPointer[j - 1] = tmp;
+			}
+		}
+	}
+}
+
+//Быстрая сортировка
+void QuickSort(double* array, int const n)
+{
+	int const lenD = n;
+	int pivot = 0;
+	int ind = lenD / 2;
+	int i, j = 0, k = 0;
+	if (lenD>1) {
+		double* L = new double[lenD];
+		double* R = new double[lenD];
+		pivot = array[ind];
+		for (i = 0; i<lenD; i++) {
+			if (i != ind) {
+				if (array[i]<pivot) {
+					L[j] = array[i];
+					j++;
+				}
+				else {
+					R[k] = array[i];
+					k++;
+				}
+			}
+		}
+		QuickSort(L, j);
+		QuickSort(R, k);
+		for (int cnt = 0; cnt<lenD; cnt++) {
+			if (cnt<j) {
+				array[cnt] = L[cnt];;
+			}
+			else if (cnt == j) {
+				array[cnt] = pivot;
+			}
+			else {
+				array[cnt] = R[cnt - (j + 1)];
+			}
+		}
+	}
+}
+
+//Сортировка выборкой
+void SelectionSort(double array[], int lenD)
+{
+	int j = 0;
+	int tmp = 0;
+	for (int i = 0; i<lenD; i++) {
+		j = i;
+		for (int k = i; k<lenD; k++) {
+			if (array[j]>array[k]) {
+				j = k;
+			}
+		}
+		tmp = array[i];
+		array[i] = array[j];
+		array[j] = tmp;
+	}
+}
+
+//Вывод массива на экран
+void ArrayShow(double array[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << array[i] << " ";
+	}
+}
+
+
+//Заполнение матриц
+int** MatrixValues(int row, int col, int **matrix)
+{
+	for (int i = 0; i < row; i++)
+	{
+		matrix[i] = new int[col];
+	}
+	for (int i = 0; i< row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			matrix[i][j] = rand() % 10;
+			cout << matrix[i][j] << "  ";
+		}
+		cout << endl;
+	}
+	return matrix;
+}
+
+//Перемножение матриц разного размера
+int** MultiplyMatrix(int n, int w, int m, int** aMatrix, int** bMatrix, int** result)
+{
+	int temp;
+	for (int row = 0; row < n; row++) {
+		for (int col = 0; col < w; col++) 
+		{
+			for (int inner = 0; inner < m; inner++) 
+			{
+				temp = aMatrix[row][inner] * bMatrix[inner][col];
+				result[row][col] = result[row][col] + temp;
+			}
+		}
+	}
+	return result;
 }
